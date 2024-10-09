2758cca4eed520d6c8b6aa1b35c83810b8fc934c
bijan_pradhananga <bijan2059@gmail.com>
2024-10-09 15:33:03 +0545

    made navbar

--- src/components/UI/Navbar.tsx
+++ src/components/UI/Navbar.tsx
@@ -1,28 +1,29 @@
+import ThemeSwitcher from '../Theme/ThemeSwitcher';
 import PhoneNavbar from './PhoneNavbar';
 
 const Navbar = () => {
 
     return (
-        <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
+        <nav className="bg-white dark:bg-gray-900 w-full z-20">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                <NavLogo/>
-                <NavLinks/>
-                <NavIcons/>
+                <NavLogo />
+                <NavLinks />
+                <NavIcons />
             </div>
         </nav>
     );
 };
 
-const NavLogo = () =>{
+const NavLogo = () => {
     return (
         <a
-        href="https://flowbite.com/"
-        className="flex items-center space-x-3 rtl:space-x-reverse"
-    >
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            BIJAN
-        </span>
-    </a>
+            href="https://flowbite.com/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+        >
+            <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+                BIJAN
+            </span>
+        </a>
     )
 }
 
@@ -30,7 +31,6 @@ const NavLinks = () => {
     return (
         <div
             className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-            id="navbar-sticky"
         >
             <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                 <li>
@@ -55,6 +55,14 @@ const NavLinks = () => {
                         href="#"
                         className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
                     >
+                        Skills
+                    </a>
+                </li>
+                <li>
+                    <a
+                        href="#"
+                        className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                    >
                         Services
                     </a>
                 </li>
@@ -71,17 +79,12 @@ const NavLinks = () => {
     )
 }
 
-const NavIcons = () =>{
+const NavIcons = () => {
     return (
-        <div className="flex md:order-2 md:space-x-0 rtl:space-x-reverse">
-        <button
-            type="button"
-            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-        >
-            Get started
-        </button>
-        <PhoneNavbar />
-    </div>
+        <div className="flex gap-2 md:gap-0 md:order-2 md:space-x-0 rtl:space-x-reverse">
+            <ThemeSwitcher />
+            <PhoneNavbar />
+        </div>
     )
 }
 

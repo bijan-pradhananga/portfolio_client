540ea8cddb2995a3a6ee9d4db7495c886472db1f
bijan_pradhananga <bijan2059@gmail.com>
2024-10-09 19:22:24 +0545

    added skills

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Home from "@/components/Pages/Home/Home";
+import Skills from "@/components/Pages/Skills/Skills";
 
 export default function Page() {
   return (
     <div className="mx-auto w-full max-w-screen-xl px-2 md:px-1">
-      <Home/>
+      <Home />
+      <div id="skills" className="py-10 lg:py-0"></div>
+      <Skills />
     </div>
   );
 }

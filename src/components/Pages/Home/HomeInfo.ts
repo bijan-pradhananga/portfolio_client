0442dfb02f91a164582b4a1e23d51c0b3d6e0cca
bijan_pradhananga <bijan2059@gmail.com>
2024-10-09 16:00:33 +0545

    added homeinfo.ts

---
+++ src/components/Pages/Home/HomeInfo.ts
@@ -0,0 +1,13 @@
+type HomeInfoType = {
+    name:string,
+    profession:string,
+    description:string,
+}
+
+export const homeInfo:HomeInfoType = {
+    name: 'Bijan Pradhananga',
+    profession: 'FullStack Developer',
+    description: `Lorem ipsum dolor sit amet consectetur erro
+        ra officiis, quis praesentium perferendis nam quas 
+        magni ea autem ipsa obcaecati temporibus totam doloribus molestias.`
+}

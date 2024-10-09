33b89dd3e08d50322badb095eb9b1e5cbbb98c70
bijan_pradhananga <bijan2059@gmail.com>
2024-10-09 15:33:12 +0545

    made header

---
+++ src/components/UI/Header.tsx
@@ -0,0 +1,11 @@
+import Navbar from './Navbar'
+
+const Header = () => {
+  return (
+    <div className="w-full dark:bg-gray-900  dark:border-b dark:border-black sticky top-0 z-50 shadow-[rgba(17,_17,_26,_0.1)_0px_0px_16px]">
+        <Navbar/>
+    </div>
+  )
+}
+
+export default Header

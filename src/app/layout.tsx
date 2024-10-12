bfc4d9d1dc0f50778f395e06aaa3f58a89ea4d57
bijan_pradhananga <bijan2059@gmail.com>
2024-10-12 07:07:28 +0545

    added footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/UI/Header";
 import { Providers as ThemeProvider } from "@/provider/ThemeProvider";
+import Footer from "@/components/UI/Footer";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -33,6 +34,7 @@ export default function RootLayout({
         <ThemeProvider>
           <Header />
           {children}
+          <Footer />
         </ThemeProvider>
       </body>
     </html>

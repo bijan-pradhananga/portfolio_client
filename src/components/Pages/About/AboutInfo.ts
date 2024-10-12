31ff99840448d68143c21960284483e811c50379
bijan_pradhananga <bijan2059@gmail.com>
2024-10-12 07:08:14 +0545

    made about info

---
+++ src/components/Pages/About/AboutInfo.ts
@@ -0,0 +1,11 @@
+type AboutInfoType = {
+    imageLink:string,
+    description:string,
+}
+
+export const aboutInfo:AboutInfoType = {
+    imageLink: '/about.png',
+    description: `Lorem ipsum dolor sit amet consectetur erro
+        ra officiis, quis praesentium perferendis nam quas 
+        magni ea autem ipsa obcaecati temporibus totam doloribus molestias.`
+}

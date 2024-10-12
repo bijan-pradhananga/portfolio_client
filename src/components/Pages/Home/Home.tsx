6289b69c9c9148a40843624768886bd9537ab4d8
Bijan <bijan2059@gmail.com>
2024-10-12 11:53:26 +0545

    separated home icons

--- src/components/Pages/Home/Home.tsx
+++ src/components/Pages/Home/Home.tsx
@@ -2,15 +2,14 @@ import Image from "next/image";
 import Link from "next/link";
 import { BsMouse } from "react-icons/bs";
 import { FaChevronDown, FaInstagram } from "react-icons/fa";
-import { LuGithub } from "react-icons/lu";
-import { PiLinkedinLogo, PiReadCvLogo } from "react-icons/pi";
-import { homeInfo } from "./HomeInfo";
+import { homeInfo, socialType } from "./HomeInfo";
+import { PiReadCvLogo } from "react-icons/pi";
 
 const Home = () => {
-  return ( 
+  return (
     <section id="home" className="lg:h-screen relative flex flex-col md:items-center lg:justify-center  dark:bg-gray-900 px-4">
       <div id="homeContainer" className=" grid grid-cols-2 lg:flex gap-x-2 gap-y-12 justify-center mt-5 lg:-mt-24 lg:gap-28">
-        <HomeIcons />
+        <HomeIconsCollection />
         <HomeImage />
         <HomeDetails />
       </div>
@@ -18,21 +17,23 @@ const Home = () => {
   );
 };
 
-const HomeIcons = () => {
+const HomeIconsCollection = () => {
   return (
     <div className="flex flex-col justify-center gap-6 text-2xl mr-2 dark:text-white">
-      <Link href="https://www.instagram.com/bijan_pradhananga/">
-        <FaInstagram />
-      </Link>
-      <Link href="https://www.linkedin.com/in/bijan-pradhananga-947b992ab/">
-        <PiLinkedinLogo />
-      </Link>
-      <Link href="https://github.com/bijan-pradhananga">
-        <LuGithub />
-      </Link>
+      {homeInfo.socials.map((social,index)=>(
+        <HomeIcon social={social} key={index}/>
+      ))}
     </div>
   );
 };
+
+const HomeIcon = ({social}:{social:socialType}) => {
+  return (
+    <Link href={social.link}>
+      <social.icon />
+    </Link>
+  )
+}
 
 const HomeDetails = () => {
   return (
@@ -44,14 +45,14 @@ const HomeDetails = () => {
       </p>
       <button className="mt-8 p-4 lg:p-5 flex gap-2 bg-black dark:bg-gray-800 text-white rounded-lg hover:bg-gray-800 dark:hover:bg-gray-950 transition ease-in">
         <span>See Resume</span>
-         <PiReadCvLogo className="text-2xl"/>
+        <PiReadCvLogo className="text-2xl" />
       </button>
       <Link href='#about' className="hidden lg:flex mt-28 p-2 gap-2 absolute hover:bg-gray-100 dark:hover:bg-gray-800 transition ease-in dark:text-white font-semibold rounded-lg ">
-          <BsMouse className="text-2xl"/>
-         <span className="mr-2">Scroll Down</span>
-         <FaChevronDown className="mt-1"/>
+        <BsMouse className="text-2xl" />
+        <span className="mr-2">Scroll Down</span>
+        <FaChevronDown className="mt-1" />
       </Link>
-  
+
     </div>
   );
 };
@@ -59,7 +60,7 @@ const HomeDetails = () => {
 const HomeImage = () => {
   return (
     <div className="overflow-hidden -ml-28 lg:-ml-6 lg:md:order-3">
-      <Image src="/profile.jpg" width={250} height={250} alt="profile" className="rounded-full"/>
+      <Image src={homeInfo.image} width={250} height={250} alt="profile" className="rounded-full" />
     </div>
   );
 };

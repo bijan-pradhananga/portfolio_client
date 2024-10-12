d5717e48bd8a2a7dd1d5842cfc0203d81f6365c4
bijan_pradhananga <bijan2059@gmail.com>
2024-10-12 07:07:55 +0545

    added other pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import About from "@/components/Pages/About/About";
+import Contact from "@/components/Pages/Contact/Contact";
 import Home from "@/components/Pages/Home/Home";
 import Skills from "@/components/Pages/Skills/Skills";
 
@@ -5,8 +7,12 @@ export default function Page() {
   return (
     <div className="mx-auto w-full max-w-screen-xl px-2 md:px-1">
       <Home />
-      <div id="skills" className="py-10 lg:py-0"></div>
+      <div id="about" className="py-8"></div>
+      <About />
+      <div id="skills" className="py-10 lg:py-20"></div>
       <Skills />
+      <div id="contact" className="py-10 lg:py-20"></div>
+      <Contact />
     </div>
   );
 }

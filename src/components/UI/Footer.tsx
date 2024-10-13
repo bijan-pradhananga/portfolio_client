a7b19dbce27a6cc623ee13f87650bec247220ca9
Bijan <bijan2059@gmail.com>
2024-10-13 08:34:02 +0545

    update footer

--- src/components/UI/Footer.tsx
+++ src/components/UI/Footer.tsx
@@ -1,10 +1,10 @@
 import Link from 'next/link'
 import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
-let year:string = new Date().getFullYear().toString();
+const year:string = new Date().getFullYear().toString();
 
 const Footer = () => {
     return (
-        <footer className="footer footer-center bg-gray-100 dark:bg-gray-800 dark:text-white text-base-content rounded p-10 mt-20">
+        <footer className="footer footer-center bg-gray-100 dark:bg-gray-800 dark:text-white text-base-content rounded p-10 mt-20 md:mt-32">
             <nav className="grid grid-flow-col gap-4">
                 <Link href='#' className="link link-hover">Home</Link>
                 <Link href='#about' className="link link-hover">About</Link>

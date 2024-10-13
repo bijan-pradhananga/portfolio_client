9d44fbcecc9221169dbc315711165f5104794599
Bijan <bijan2059@gmail.com>
2024-10-13 08:40:58 +0545

    added projects

--- src/components/Pages/Project/Project.tsx
+++ src/components/Pages/Project/Project.tsx
@@ -23,20 +23,23 @@ const ProjectHeader = () => {
 const ProjectsContainer = () => {
   return (
     <main className='grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3'>
-      {projects.map((project,index)=>(
-        <ProjectComponent project={project} key={index}/>
+      {projects.map((project, index) => (
+        <ProjectComponent project={project} key={index} />
       ))}
     </main >
   )
 }
 
-const ProjectComponent = ({project}:{project:projectType}) => {
+const ProjectComponent = ({ project }: { project: projectType }) => {
   return (
     <div className='flex flex-col gap-2 shadow-lg px-5 py-8 rounded-lg dark:bg-gray-800'>
-      <Image src={project.image} alt="img" width={400} height={200} className='w-full rounded-lg shadow-md'/>
+      <div className='w-full bg-gray-300 aspect-video rounded-lg'>
+        <Image src={project.image} width={400} height={10} alt={project.name} className='w-full rounded shadow-lg object-cover dark:filter dark:brightness-90 dark:border'/>
+      </div>
       <h2 className='text-xl font-semibold text-gray-600 mt-2 dark:text-gray-400'>{project.name}</h2>
       <Link href={project.link} className='text-gray-500 font-semibold'>Demo →</Link>
     </div>
+
   )
 }
 
--- src/components/Pages/Project/ProjectInfo.ts
+++ src/components/Pages/Project/ProjectInfo.ts
@@ -5,7 +5,9 @@ export interface projectType{
 }
 
 export const projects:projectType[] = [
-    {name:'Speed Design',image:'/image.png',link:'https://speed-client.vercel.app/'},
-    {name:'Speed Design',image:'/image.png',link:'https://speed-client.vercel.app/'},
-    {name:'Speed Design',image:'/image.png',link:'https://speed-client.vercel.app/'},
+    {name:'Speed Design',image:'/projects/speed.png',link:'https://speed-client.vercel.app/'},
+    {name:'Simple ChatApp',image:'/projects/chatApp.png',link:'https://chat-app-client-cyan.vercel.app/'},
+    {name:'Music Player',image:'/projects/musicPlayer.png',link:'https://bijan-musicplayer.netlify.app'},
+    {name:'Simple Piano',image:'/projects/simplePiano.png',link:'https://simple-pianofy.netlify.app'},
+    {name:'Simple Calculator',image:'/projects/calc.png',link:'https://bijan-calc.netlify.app'},
 ]

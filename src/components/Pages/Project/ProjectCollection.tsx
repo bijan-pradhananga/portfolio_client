ce2444757217c45ee151adef83e8dc1e70510b2e
Bijan <bijan2059@gmail.com>
2024-10-17 07:39:38 +0545

    added loader

--- src/components/Pages/Project/ProjectCollection.tsx
+++ src/components/Pages/Project/ProjectCollection.tsx
@@ -6,32 +6,49 @@ import Link from "next/link";
 
 const ProjectCollection = () => {
     const [projects, setProjects] = useState<projectType[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [error, setError] = useState(false);
+
     const fetchProjects = async () => {
+        setIsLoading(true);
+        setError(false); // Reset error state before new request
         try {
             const res = await fetch('https://portfolio-server-qjh8.onrender.com/project', {
-                cache: 'no-store'
+                cache: 'no-store',
             });
             if (!res.ok) {
                 throw new Error('Failed to fetch projects');
             }
             const projectData: ProjectApiResponse = await res.json();
+            setIsLoading(false);
             setProjects(projectData.projects); // Update state with latest projects
         } catch (error) {
+            setIsLoading(false);
+            setError(true);
             console.error('Error fetching projects:', error);
         }
     };
+
     useEffect(() => {
         fetchProjects();
-    }, []); 
+    }, []);
+
+    if (error) {
+        return <p>Error fetching projects. Please try again later.</p>;
+    }
 
     return (
         <main className='grid grid-cols-1 gap-4 md:grid-cols-2 xl:grid-cols-3'>
-            {projects.map((project, index) => (
+           {isLoading &&
+             <> <ProjectLoader /> <ProjectLoader /> <ProjectLoader /></>
+           }
+            {!isLoading && !error && projects.map((project, index) => (
                 <ProjectComponent project={project} key={index} />
             ))}
-        </main >
-    )
-}
+        </main>
+    );
+};
+
 
 const ProjectComponent = ({ project }: { project: projectType }) => {
     return (
@@ -44,6 +61,19 @@ const ProjectComponent = ({ project }: { project: projectType }) => {
         </div>
 
     )
+}
+
+const ProjectLoader = () => {
+    return (
+        <div className='flex flex-col gap-2 shadow-lg px-5 py-8 rounded-lg dark:bg-gray-800'>
+            <div className='w-full skeleton dark:bg-gray-600 aspect-video rounded-lg mb-2'>
+
+            </div>
+            <div className="skeleton h-6 w-24 rounded dark:bg-gray-600"></div>
+            <div className="skeleton h-5 w-20 rounded dark:bg-gray-600"></div>
+        </div>
+    )
+
 }
 
 export default ProjectCollection

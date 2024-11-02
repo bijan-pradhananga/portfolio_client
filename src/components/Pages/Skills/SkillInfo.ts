a416d3669f1bfedee076bb481918f3b62dbb476a
bijan_pradhananga <bijan2059@gmail.com>
2024-11-02 08:45:34 +0545

    added skills

--- src/components/Pages/Skills/SkillInfo.ts
+++ src/components/Pages/Skills/SkillInfo.ts
@@ -1,11 +1,12 @@
 import { IconType } from 'react-icons';  
-import { DiPhp } from 'react-icons/di';
+import { BiLogoTypescript } from 'react-icons/bi';
+import { DiPhp, DiVisualstudio } from 'react-icons/di';
 import { FaGithub, FaGitAlt } from 'react-icons/fa';
-import { FaHtml5, FaBootstrap, FaReact, FaCss3Alt, FaJs, FaNodeJs, FaLaravel, FaPython } from 'react-icons/fa6';
+import { FaHtml5, FaBootstrap, FaReact, FaCss3Alt, FaJs, FaNodeJs, FaLaravel, FaPython, FaJava } from 'react-icons/fa6';
 import { FiFigma } from 'react-icons/fi';
 import { GrMysql } from 'react-icons/gr';
 import { RiNextjsFill, RiTailwindCssFill } from 'react-icons/ri';
-import { SiMongodb, SiExpress, SiLinux, SiDaisyui, SiAdobephotoshop, SiCanva } from 'react-icons/si';
+import { SiMongodb, SiExpress, SiLinux, SiDaisyui, SiAdobephotoshop, SiCanva, SiDotnet, SiMui, SiShadcnui } from 'react-icons/si';
 import { TbBrandRedux } from 'react-icons/tb';
 import { VscVscode } from 'react-icons/vsc';
 
@@ -34,16 +35,19 @@ export const skills: SkillCategory[] = [
       { name: "Redux", icon: TbBrandRedux },
       { name: "NextJS", icon: RiNextjsFill },
       { name: "Daisy UI", icon: SiDaisyui },
+      { name: "Material UI", icon: SiMui },
+      { name: "Shadcn UI", icon: SiShadcnui },
     ]
   },
   {
     title: "Backend Skills",
     skills: [
       { name: 'PHP', icon: DiPhp },
-      { name: 'Python', icon: FaPython },
       { name: "Node.js", icon: FaNodeJs },
       { name: "Express", icon: SiExpress },
+      { name: 'Python', icon: FaPython },
       { name: 'Laravel', icon: FaLaravel },
+      { name: 'ASP.Net', icon: SiDotnet },
       { name: "MongoDB", icon: SiMongodb },
       { name: "MySQL", icon: GrMysql },
     ]
@@ -54,7 +58,10 @@ export const skills: SkillCategory[] = [
       { name: "Git", icon: FaGitAlt },
       { name: "GitHub", icon: FaGithub },
       { name: "Linux", icon: SiLinux },
+      { name: "Java", icon: FaJava },
+      { name: 'TypeScript', icon: BiLogoTypescript},
       { name: "VS Code", icon: VscVscode },
+      { name: "Visual Studio", icon: DiVisualstudio },
       { name: "PhotoShop", icon: SiAdobephotoshop },
       { name: "Figma", icon: FiFigma },
       { name: "Canva", icon: SiCanva },
